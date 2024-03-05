8c042407726d0eb2cf36d747f8abe2a8ea762449
Viktor <strik4462@gmail.com>
2024-03-05 11:19:42 +0300

    added data interceptor

---
+++ AssignmentApp.Persistence/Interceptors/DataInterceptor.cs
@@ -0,0 +1,26 @@
+﻿using AssignmentApp.Application.Interfaces;
+using AssignmentApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace AssignmentApp.Persistence.Interceptors
+{
+    public class DataInterceptor : SaveChangesInterceptor
+    {
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            var dbContext = eventData.Context;
+            if (dbContext is null)
+                return base.SavingChangesAsync(eventData, result, cancellationToken);
+
+            var entries = dbContext.ChangeTracker.Entries<Assignment>();
+
+            foreach (var entry in entries)
+            {
+                if (entry.State == EntityState.Added)
+                    entry.Property(x => x.DateCreated).CurrentValue = DateTime.UtcNow;
+            }
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+    }
+}
